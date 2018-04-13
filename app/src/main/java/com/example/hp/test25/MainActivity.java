48e4e786ce4fd57e40ce965e27f272bb3b585e5e
yangkailun <2362080354@qq.com>
2018-04-13 11:48:35 +0800

    second commit,finish toolbar

--- app/src/main/java/com/example/hp/test25/MainActivity.java
+++ app/src/main/java/com/example/hp/test25/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         initView();
     }
 
