b8f28f950287b97fc3cc666e1e65bd4511a041a1
yangkailun <2362080354@qq.com>
2018-04-19 11:20:08 +0800

    完成点击查看收支详情

--- app/src/main/java/com/example/hp/test25/adapter/DealAdapter.java
+++ app/src/main/java/com/example/hp/test25/adapter/DealAdapter.java
@@ -1,5 +1,6 @@
 package com.example.hp.test25.adapter;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.hp.test25.R;
 import com.example.hp.test25.object.Deal;
 import com.example.hp.test25.util.TimeUti;
@@ -22,11 +24,13 @@ public class DealAdapter extends RecyclerView.Adapter<DealAdapter.ViewHolder> {
     private List<Deal> mDealList;
 
     static class ViewHolder extends RecyclerView.ViewHolder{
+        CardView dealCardView;
         ImageView dealImage,typeImage;
         TextView money,time;
 
         public ViewHolder(View view){
             super(view);
+            dealCardView = view.findViewById(R.id.deal_cardview);
             dealImage = view.findViewById(R.id.deal_image);
             money = view.findViewById(R.id.money_item);
             typeImage = view.findViewById(R.id.type_image);
@@ -39,12 +43,62 @@ public class DealAdapter extends RecyclerView.Adapter<DealAdapter.ViewHolder> {
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType){
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.deal,parent,false);
-        ViewHolder holder = new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
 
 
+        holder.dealCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Deal deal = mDealList.get(position);
+                boolean wrapInScrollView = true;
+                MaterialDialog dialog = new MaterialDialog.Builder(parent.getContext())
+                        .title("详细")
+                        .customView(R.layout.detail_deal,wrapInScrollView)
+                        .positiveText("确定")
+                        .build();
+                TextView detail_direction = dialog.getCustomView().findViewById(R.id.detail_direction);
+                TextView detail_money = dialog.getCustomView().findViewById(R.id.detail_money);
+                TextView detail_type = dialog.getCustomView().findViewById(R.id.detail_type);
+                TextView detail_remark = dialog.getCustomView().findViewById(R.id.detail_remark);
+                TextView detail_time = dialog.getCustomView().findViewById(R.id.detail_time);
+                detail_direction.setText(deal.getDirection()==Deal.EXPENSES?"支出":"收入");
+                detail_money.setText(deal.getMoney()+"");
+                switch (deal.getType()){
+                    case 2:
+                        detail_type.setText("衣");
+                        break;
+                    case 3:
+                        detail_type.setText("食");
+                        break;
+                    case 4:
+                        detail_type.setText("住");
+                        break;
+                    case 5:
+                        detail_type.setText("行");
+                        break;
+                    case 6:
+                        detail_type.setText("其他支出");
+                        break;
+                    case 7:
+                        detail_type.setText("职业收入");
+                        break;
+                    case 8:
+                        detail_type.setText("投资收入");
+                        break;
+                    case 9:
+                        detail_type.setText("其他收入");
+                        break;
+                    default:
+                }
+                detail_remark.setText(deal.getRemark());
+                detail_time.setText(TimeUti.outPutTime(deal.getTime()));
+                dialog.show();
+            }
+        });
 
         return holder;
     }
